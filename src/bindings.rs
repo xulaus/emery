2ab160e7ac834575231a84d933945edebb10dbb5
Richard Fitzgerald <richard.fitzgerald@zappistore.com>
2021-07-25 13:12:52 +0100

    Add first function and needed scaffolding

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,22 +1,112 @@
 extern crate libc;
+use std::{error::Error, ffi::CString, result::Result};
 
-mod ffi {
-    use libc::c_double;
+mod ruby {
     pub use libc::uintptr_t as VALUE;
+    use libc::{c_double, c_int, c_void};
+
+    pub type CallbackPtr = *const c_void;
 
     #[link(name = "ruby")]
     extern "C" {
-        pub fn rb_define_global_const(name: *const u8, value: VALUE);
+        pub fn rb_define_global_const(name: *const i8, value: VALUE);
         pub fn rb_float_new(value: c_double) -> VALUE;
+        pub fn rb_num2dbl(value: VALUE) -> c_double;
+
+        pub fn rb_define_module(name: *const i8) -> VALUE;
+        pub fn rb_define_module_under(parent: VALUE, name: *const i8) -> VALUE;
+
+        pub fn rb_define_method(parent: VALUE, name: *const i8, func: *const c_void, argc: c_int);
+        pub fn rb_define_module_function(
+            parent: VALUE,
+            name: *const i8,
+            func: *const c_void,
+            argc: c_int,
+        );
+        pub fn rb_define_global_function(name: *const i8, func: *const c_void, argc: c_int);
     }
 }
 
-pub use ffi::VALUE;
+pub use ruby::VALUE;
 
-pub fn rb_float_new(value: f64) -> VALUE {
-    unsafe { ffi::rb_float_new(value) }
+pub trait RubyCallback {
+    const ARGC: i32;
+    fn as_ruby(&self) -> ruby::CallbackPtr;
 }
 
-pub fn rb_define_global_const(name: &str, value: VALUE) {
-    unsafe { ffi::rb_define_global_const(name.as_bytes().as_ptr(), value) }
+impl RubyCallback for extern "C" fn() -> VALUE {
+    const ARGC: i32 = 0;
+
+    fn as_ruby(&self) -> ruby::CallbackPtr {
+        *self as ruby::CallbackPtr
+    }
+}
+
+impl RubyCallback for extern "C" fn(VALUE) -> VALUE {
+    const ARGC: i32 = 1;
+
+    fn as_ruby(&self) -> ruby::CallbackPtr {
+        *self as ruby::CallbackPtr
+    }
+}
+
+#[allow(dead_code)]
+pub fn rb_float_new(value: f64) -> VALUE {
+    unsafe { ruby::rb_float_new(value) }
+}
+
+#[allow(dead_code)]
+pub fn rb_define_global_const(name: &str, value: VALUE) -> Result<(), Box<dyn Error>> {
+    let c_name = CString::new(name)?;
+    unsafe { ruby::rb_define_global_const(c_name.as_ptr(), value) };
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub fn rb_num2dbl(value: VALUE) -> f64 {
+    unsafe { ruby::rb_num2dbl(value) }
+}
+
+#[allow(dead_code)]
+pub fn rb_define_module(name: &str) -> Result<VALUE, Box<dyn Error>> {
+    let c_name = CString::new(name)?;
+    Ok(unsafe { ruby::rb_define_module(c_name.as_ptr()) })
+}
+
+#[allow(dead_code)]
+pub fn rb_define_module_under(parent: VALUE, name: &str) -> Result<VALUE, Box<dyn Error>> {
+    let c_name = CString::new(name)?;
+    Ok(unsafe { ruby::rb_define_module_under(parent, c_name.as_ptr()) })
+}
+
+#[allow(dead_code)]
+pub fn rb_define_method<F: RubyCallback>(
+    parent: VALUE,
+    name: &str,
+    func: F,
+) -> Result<(), Box<dyn Error>> {
+    let c_name = CString::new(name)?;
+    unsafe { ruby::rb_define_method(parent, c_name.as_ptr(), func.as_ruby(), F::ARGC) };
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub fn rb_define_module_function<F: RubyCallback>(
+    parent: VALUE,
+    name: &str,
+    func: F,
+) -> Result<(), Box<dyn Error>> {
+    let c_name = CString::new(name)?;
+    unsafe { ruby::rb_define_module_function(parent, c_name.as_ptr(), func.as_ruby(), F::ARGC) };
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub fn rb_define_global_function<F: RubyCallback>(
+    name: &str,
+    func: F,
+) -> Result<(), Box<dyn Error>> {
+    let c_name = CString::new(name)?;
+    unsafe { ruby::rb_define_global_function(c_name.as_ptr(), func.as_ruby(), F::ARGC) };
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 mod bindings;
-use bindings::{rb_define_global_const, rb_float_new, VALUE};
+use bindings::*;
+
+#[no_mangle]
+pub extern "C" fn emery_fn(_a: VALUE) -> VALUE {
+    rb_float_new(2.0)
+}
 
 #[no_mangle]
 pub extern "C" fn Init_libemery() {
-    rb_define_global_const("EMERY", rb_float_new(1.0));
+    rb_define_global_const("EMERY", rb_float_new(1.0)).expect("invalid function name");
+    rb_define_global_function("emery_fn", emery_fn as extern "C" fn(VALUE) -> VALUE)
+        .expect("invalid function name");
 }
