c6455ea139715a420439679fd82ef2bfcdfb7208
Richard Fitzgerald <richard.fitzgerald@zappistore.com>
2022-07-22 08:23:05 +0100

    More bit sizes

--- examples/fnv/ext/fnv/src/lib.rs
+++ examples/fnv/ext/fnv/src/lib.rs
@@ -2,7 +2,7 @@ extern crate emery;
 
 use emery::*;
 
-fn fnv1a (arg: RubyValue) -> Result<String, RubyConversionError>{
+fn fnv1a64(arg: RubyValue) -> Result<String, RubyConversionError>{
     let string: RubyStringLike = TryFromRuby::try_from(&arg)?;
     const PRIME: u64 = 1099511628211;
     const BASIS: u64 = 14695981039346656037;
@@ -16,19 +16,41 @@ fn fnv1a (arg: RubyValue) -> Result<String, RubyConversionError>{
     Ok(format!("{:x}", hash))
 }
 
+fn fnv1a128(arg: RubyValue) -> Result<String, RubyConversionError>{
+    let string: RubyStringLike = TryFromRuby::try_from(&arg)?;
+    const PRIME: u128 = 309485009821345068724781371;
+    const BASIS: u128 = 144066263297769815596495629667062367629;
+    let hash = string.bytes().iter().fold(
+        BASIS,
+        |hash, byte| {
+            (hash ^ (*byte as u128)).wrapping_mul(PRIME)
+        }
+    );
+    Ok(format!("{:x}", hash))
+}
+
 #[no_mangle]
-pub extern "C" fn ruby_str_fnv1a(_module: RubyValue, arg: RubyValue) -> RubyValue {
-    fnv1a(arg).into()
+pub extern "C" fn ruby_str_fnv1a64(_module: RubyValue, arg: RubyValue) -> RubyValue {
+    fnv1a64(arg).into()
+}
+#[no_mangle]
+pub extern "C" fn ruby_str_fnv1a128(_module: RubyValue, arg: RubyValue) -> RubyValue {
+    fnv1a128(arg).into()
 }
 
 #[no_mangle]
 pub extern "C" fn Init_fnv() {
-    let mut emery_module = rb_define_module("EMERY").expect("invalid module name");
-    let mut fnv1a = rb_define_module_under(&mut emery_module, "FNV1a").expect("invalid module name");
+    let mut emery_module = rb_define_module("Digest").expect("invalid module name");
+    let mut fnv1a64 = rb_define_module_under(&mut emery_module, "FNV1a64").expect("invalid module name");
     rb_define_module_function(
-        &mut fnv1a,
+        &mut fnv1a64,
         "hexdigest",
-        ruby_str_fnv1a as extern "C" fn(RubyValue, RubyValue) -> RubyValue,
-    )
-    .expect("invalid function name");
+        ruby_str_fnv1a64 as extern "C" fn(RubyValue, RubyValue) -> RubyValue,
+    ).expect("invalid function name");
+    let mut fnv1a128 = rb_define_module_under(&mut emery_module, "FNV1a128").expect("invalid module name");
+    rb_define_module_function(
+        &mut fnv1a128,
+        "hexdigest",
+        ruby_str_fnv1a128 as extern "C" fn(RubyValue, RubyValue) -> RubyValue,
+    ).expect("invalid function name");
 }
--- examples/fnv/tests/fnv1a_test.rb
+++ examples/fnv/tests/fnv1a_test.rb
@@ -1,22 +1,42 @@
 require "fnv"
 require 'minitest/autorun'
 
-describe EMERY::FNV1a do
+describe Digest::FNV1a64 do
   describe "whens given non string data" do
     it "raises an error" do
-      assert_raises(RuntimeError){ EMERY::FNV1a.hexdigest(nil) }
+      assert_raises(RuntimeError){ Digest::FNV1a64.hexdigest(nil) }
     end
   end
 
   describe "whens given symbol data" do
     it "raises an error" do
-      assert_equal "2da9bc21757082d4", EMERY::FNV1a.hexdigest(:adsadsa)
+      assert_equal "2da9bc21757082d4", Digest::FNV1a64.hexdigest(:adsadsa)
     end
   end
 
   describe "whens given string data" do
     it "returns expected hash" do
-      assert_equal "2da9bc21757082d4", EMERY::FNV1a.hexdigest("adsadsa")
+      assert_equal "2da9bc21757082d4", Digest::FNV1a64.hexdigest("adsadsa")
+    end
+  end
+end
+
+describe Digest::FNV1a128 do
+  describe "whens given non string data" do
+    it "raises an error" do
+      assert_raises(RuntimeError){ Digest::FNV1a128.hexdigest(nil) }
+    end
+  end
+
+  describe "whens given symbol data" do
+    it "raises an error" do
+      assert_equal "7dd10dc6da4ff78da7c74cad91c6ee6c", Digest::FNV1a128.hexdigest(:adsadsa)
+    end
+  end
+
+  describe "whens given string data" do
+    it "returns expected hash" do
+      assert_equal "7dd10dc6da4ff78da7c74cad91c6ee6c", Digest::FNV1a128.hexdigest("adsadsa")
     end
   end
 end
